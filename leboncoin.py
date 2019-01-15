1462563b69e8dc55536816ca39438573b55ac0c0
Clement Lemaire <lemairec02@gmail.com>
2019-01-15 10:25:28 +0100

    Scrapper ok

--- leboncoin.py
+++ leboncoin.py
@@ -78,6 +78,88 @@ def scrappe_leboncoin(url, category, remark = ""):
     #print mydatas;
     saveOrUpdate(mydatas);
 
+def save(page):
+    f= open("agriaffaires.html","w+")
+    f.write(page.encode('utf-8'))
+    f.close();
+
+def load():
+    f= open("agriaffaires.html","r")
+    contents =f.read()
+    f.close();
+    return contents
+
+
+def scrappe_agriaffaire(url):
+    print url
+    driver.get(url)
+    page = driver.page_source
+    analyse_agriaffaire(page)
+
+
+    #requete = requests.get("https://zestedesavoir.com/tutoriels/?category=autres-informatique")
+    #page = requete.content
+
+    #print page.encode('utf-8')
+
+
+
+def stringlify(str):
+    str = str.replace("\n", "")
+    str = str.replace("    ", " ")
+    str = str.replace("    ", " ")
+    str = str.replace("    ", " ")
+    str = str.replace("   ", " ")
+    str = str.replace("  ", " ")
+    str = str.replace(" ", " ")
+    return str
+
+def analyse_agriaffaire(page):
+    soup = BeautifulSoup(page, "html.parser")
+    divs = soup.find_all("div", {"class": "listing--element"})
+
+    print len(divs)
+    mydatas = []
+    for div in divs:
+        #print("\n\n######\n")
+        #print(div)
+        #print("\n\n######\n")
+
+
+
+        try:
+            type = "agriaffaire"
+            category = ""
+            title = div.a.div.text
+            price = 0;
+            div_price = div.find("div", {"class": "price"})
+            if(div_price):
+                text = div_price.span.text.replace(" ", "");
+                price = int(text)
+
+            image = ""
+            div_img = div.find("img", {"class": "loaded"})
+            if(div_img):
+                image = div_img.attrs["data-src"]
+            url = "https://www.agriaffaires.com"+div.a.attrs["href"]
+            clientId = url
+
+            text = div.find("div", {"class": "listing--galerie--none"}).text;
+            description = stringlify(text)
+
+            mydata = {};
+            mydata["type"] = type
+            mydata["category"] = category
+            mydata["title"] = title
+            mydata["price"] = price
+            mydata["image"] = image
+            mydata["url"] = url
+            mydata["clientId"] = clientId
+            mydata["description"] = description
+            mydatas.append(mydata)
+        except Exception as e:
+            print("toto")
+    saveOrUpdate(mydatas);
 
 
 def scrappe_agriaffaires():
@@ -94,26 +176,37 @@ def scrappe_agriaffaires():
     scrappe_agriaffaire("https://www.agriaffaires.com/occasion/semoir/1-france_picardie.html");
 
 
+driver = None
 
-print("update new");
-updateNew();
+def main():
+    global driver
 
-# create a new Firefox session
-print("create driver");
-driver = webdriver.Safari()
-driver.implicitly_wait(1)
-print("driver ok");
-#wait = WebDriverWait(driver, 3);
+    print("update new");
 
-paths = ["https://www.leboncoin.fr/materiel_agricole/offres/champagne_ardenne/", "https://www.leboncoin.fr/materiel_agricole/offres/picardie/"]
-#paths = ["https://www.leboncoin.fr/materiel_agricole/offres/champagne_ardenne/"]
-for path in paths:
-	scrappe_leboncoin(path, "")
-for path in paths:
-	for i in [2,3]:
-		path2 = path + "p-" + str(i) + "/"
-		scrappe_leboncoin(path2, "")
+    updateNew();
 
-scrappe_leboncoin_appartements()
+    # create a new Firefox session
+    print("create driver");
+    driver = webdriver.Safari()
+    driver.implicitly_wait(1)
+    #print("driver ok");
+    #wait = WebDriverWait(driver, 3);
 
-driver.quit()
+    scrappe_agriaffaires();
+
+
+    paths = ["https://www.leboncoin.fr/materiel_agricole/offres/champagne_ardenne/", "https://www.leboncoin.fr/materiel_agricole/offres/picardie/"]
+    #paths = ["https://www.leboncoin.fr/materiel_agricole/offres/champagne_ardenne/"]
+    for path in paths:
+        scrappe_leboncoin(path, "")
+    	for i in [2,3]:
+    		path2 = path + "p-" + str(i) + "/"
+    		scrappe_leboncoin(path2, "")
+
+    scrappe_leboncoin_appartements()
+
+    driver.quit()
+
+main();
+#page = load()
+#analyse_agriaffaire(page)
